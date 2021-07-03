d25ee030acbd404111411e92fc121f7e1ffbec81
andrea <andreablass11@gmail.com>
2021-07-02 19:14:07 -0500

    Variables, constantes y zero values

--- src/main.go
+++ src/main.go
@@ -3,6 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	//Declaración de variables
+	//:= --> significa que creará la variable y le asignará el valor
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado: ", areaCuadrado)
 
 }
